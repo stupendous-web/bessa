c544b8c790c9ea4b8fc3f27535673dbf300c268f
stupendousweb <topher@stupendousweb.com>
2023-01-26 16:36:46 -0800

    Think twice, code once.

--- pages/app/members/index.js
+++ pages/app/members/index.js
@@ -13,6 +13,7 @@ import avatar from "../../../images/avatar.jpg";
 
 export default function Members() {
   const [users, setUsers] = useState();
+  const [isPermissionDenied, setIsPermissionDenied] = useState(false);
   const [coords, setCoords] = useState({
     latitude: undefined,
     longitude: undefined,
@@ -26,7 +27,10 @@ export default function Members() {
           longitude: position.coords.longitude,
         });
       },
-      (error) => console.log(error),
+      (error) => {
+        error.code === 1 && setIsPermissionDenied(true);
+        console.log(error);
+      },
       { enableHighAccuracy: true, timeout: 10000 }
     );
   }, []);
@@ -53,35 +57,46 @@ export default function Members() {
         <Navigation />
         <div className={"uk-section uk-section-xsmall"}>
           <div className={"uk-container"}>
-            <div
-              className={
-                "uk-child-width-1-2 uk-child-width-1-6@s uk-grid-match"
-              }
-              data-uk-grid={""}
-            >
-              {users?.map((user) => (
-                <div key={user._id}>
-                  <Link href={`/app/members/${user._id}`}>
-                    <div className={"uk-card uk-card-default uk-card-small"}>
-                      <div className={"uk-card-media-top"}>
-                        <Image src={avatar} alt={"Pride Flag"} />
-                      </div>
-                      <div className={"uk-card-body"}>
-                        <div className={"uk-text-bold"}>{user?.name}</div>
-                        {(user?.distance || user?.distance === 0) && (
+            {!isPermissionDenied ? (
+              <div
+                className={
+                  "uk-child-width-1-2 uk-child-width-1-6@s uk-grid-match"
+                }
+                data-uk-grid={""}
+              >
+                {users?.map((user) => (
+                  <div key={user._id}>
+                    <Link href={`/app/members/${user._id}`}>
+                      <div className={"uk-card uk-card-default uk-card-small"}>
+                        <div className={"uk-card-media-top"}>
+                          <Image src={avatar} alt={"Pride Flag"} />
+                        </div>
+                        <div className={"uk-card-body"}>
+                          <div className={"uk-text-bold"}>{user?.name}</div>
+                          {(user?.distance || user?.distance === 0) && (
+                            <div className={"uk-text-small uk-text-muted"}>
+                              {formatDistance(user.distance)}
+                            </div>
+                          )}
                           <div className={"uk-text-small uk-text-muted"}>
-                            {formatDistance(user.distance)}
+                            {dayjs(user?.lastActiveAt).fromNow()}
                           </div>
-                        )}
-                        <div className={"uk-text-small uk-text-muted"}>
-                          {dayjs(user?.lastActiveAt).fromNow()}
                         </div>
                       </div>
-                    </div>
-                  </Link>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className={"uk-section uk-section-xlarge"}>
+                <div
+                  className={"uk-container uk-container-expand uk-text-center"}
+                >
+                  Oops! This section doesn&apos;t work unless you share your
+                  location.
                 </div>
-              ))}
-            </div>
+              </div>
+            )}
           </div>
         </div>
       </Authentication>
