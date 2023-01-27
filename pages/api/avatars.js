7b110fa0df7ce95fe918271d190fbfbaf6057935
stupendousweb <topher@stupendousweb.com>
2023-01-26 16:22:01 -0800

    Think twice, code once.

--- pages/api/avatars.js
+++ pages/api/avatars.js
@@ -1,8 +1,24 @@
 import { MongoClient } from "mongodb";
 import { authOptions } from "./auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
+import formidable from "formidable";
+const { Storage } = require("@google-cloud/storage");
 
 export default async function handler(request, response) {
+  const form = formidable();
+  await new Promise((resolve, reject) => {
+    form.parse(request, (error, fields, files) => {
+      if (error) {
+        reject(error);
+
+        return;
+      }
+      request.fields = fields;
+      request.file = files?.file;
+      resolve();
+    });
+  });
+
   const client = new MongoClient(process.env.MONGO_DB_URI);
   const collection = client.db("bessa").collection("avatars");
   await client.connect();
@@ -11,13 +27,28 @@ export default async function handler(request, response) {
   const res = response;
   const session = await unstable_getServerSession(req, res, authOptions);
 
+  const storage = new Storage({
+    project_id: "stupendous-web",
+    credentials: {
+      client_email: process.env.GCS_CLIENT_EMAIL,
+      private_key: process.env.GCS_PRIVATE_KEY,
+    },
+  });
+
   switch (request.method) {
     case "POST":
-      await collection.insertOne({ userId: session?.user?._id }).finally(() => {
-        client.close();
-
-        response.status(200).send("Good things come to those who wait.");
-      });
+      await collection
+        .insertOne({ userId: session?.user?._id })
+        .then(async (result) => {
+          await storage.bucket("bessa").upload(file?.filepath, {
+            destination: `avatars/${result.insertedId}`,
+            contentType: file?.mimetype,
+          });
+        })
+        .then((result) => response.status(200).json(result.insertedId))
+        .finally(() => {
+          client.close();
+        });
       break;
     default:
       response.status(405).send();
