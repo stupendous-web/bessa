3c470047333d27f51b2d598c55f93763c56844d7
stupendousweb <topher@stupendousweb.com>
2023-02-08 14:34:48 -0800

    Think twice, code once.

---
+++ pages/privacy.js
@@ -0,0 +1,172 @@
+import Head from "next/head";
+
+import Navigation from "../components/Navigation";
+
+export default function Privacy() {
+  return (
+    <>
+      <Head>
+        <title>
+          Privacy | FREE gay online dating, chat, social, and listings community
+          | Welcome to the gay supper club
+        </title>
+      </Head>
+      <Navigation />
+      <div className={"uk-section"}>
+        <div className={"uk-container"}>
+          <h1>Privacy Policy</h1>
+          <p>
+            Protecting your private information is our priority. This Statement
+            of Privacy applies to Bessa and governs data collection and usage.
+            For the purposes of this Privacy Policy, unless otherwise noted, all
+            references to Bessa include https://bessssssa.com. The Bessa
+            application is a social media application. By using the Bessa
+            application, you consent to the data practices described in this
+            statement.
+          </p>
+          <div>🎉 Bessa is 100% ad-free.</div>
+          <div>🎉 Your information will not be sold.</div>
+          <div>🎉 Delete your information and media anytime.</div>
+          <div>🎉 Your information is safe!</div>
+          <div>⚠️ Bessa stores your information to work properly.</div>
+          <div>⚠️ You&apos;ll receive emails from time to time.</div>
+          <div>⚠️ Bessa stores any additional information you enter.</div>
+          <h2>Collection of your Personal Information</h2>
+          <p>
+            In order to better provide you with products and services offered,
+            Bessa may collect personally identifiable information, such as your:
+          </p>
+          <div>✅ Name</div>
+          <div>✅ Email Address</div>
+          <p>
+            We do not collect any personal information about you unless you
+            voluntarily provide it to us. However, you may be required to
+            provide certain personal information to us when you elect to use
+            certain products or services. These may include: (a) registering for
+            an account; (b) sending us an email message. To wit, we will use
+            your information for, but not limited to, communicating with you in
+            relation to services and/or products you have requested from us. We
+            also may gather additional personal or non-personal information in
+            the future.
+          </p>
+          <h2>Use of your Personal Information</h2>
+          <p>
+            Bessa collects and uses your personal information to operate and
+            deliver the services you have requested. Bessa may also use your
+            personally identifiable information to inform you of other products
+            or services available from Bessa.
+          </p>
+          <h2>Sharing Information with Third Parties</h2>
+          <p>
+            Bessa does not sell, rent or lease its customer lists to third
+            parties. Bessa may share data with trusted partners to help perform
+            statistical analysis, send you email or postal mail, provide
+            customer support. All such third parties are prohibited from using
+            your personal information except to provide these services to Bessa,
+            and they are required to maintain the confidentiality of your
+            information.
+          </p>
+          <p>
+            Bessa may disclose your personal information, without notice, if
+            required to do so by law or in the good faith belief that such
+            action is necessary to: (a) conform to the edicts of the law or
+            comply with legal process served on Bessa or the site; (b) protect
+            and defend the rights or property of Bessa; and/or (c) act under
+            exigent circumstances to protect the personal safety of users of
+            Bessa, or the public.
+          </p>
+          <h2>Right to Deletion</h2>
+          <p>
+            Subject to certain exceptions set out below, on receipt of a
+            verifiable request from you, we will:
+          </p>
+          <div>✅ Delete your personal information from our records; and</div>
+          <div>✅ Delete your personal media from our records; and</div>
+          <div>
+            ✅ Direct any service providers to delete your personal information
+            from their records.
+          </div>
+          <p>
+            Please note that we may not be able to comply with requests to
+            delete your personal information if it is necessary to:
+          </p>
+          <div>
+            ❌ Complete the transaction for which the personal information was
+            collected, fulfill the terms of a written warranty or product recall
+            conducted in accordance with federal law, provide a good or service
+            requested by you, or reasonably anticipated within the context of
+            our ongoing business relationship with you, or otherwise perform a
+            contract between you and us;
+          </div>
+          <div>
+            ❌ Detect security incidents, protect against malicious, deceptive,
+            fraudulent, or illegal activity; or prosecute those responsible for
+            that activity;
+          </div>
+          <div>
+            ❌ Debug to identify and repair errors that impair existing intended
+            functionality;
+          </div>
+          <div>
+            ❌ Exercise free speech, ensure the right of another consumer to
+            exercise his or her right of free speech, or exercise another right
+            provided for by law;
+          </div>
+          <div>
+            ❌ Comply with the California Electronic Communications Privacy Act;
+          </div>
+          <div>
+            ❌ Engage in public or peer-reviewed scientific, historical, or
+            statistical research in the public interest that adheres to all
+            other applicable ethics and privacy laws, when our deletion of the
+            information is likely to render impossible or seriously impair the
+            achievement of such research, provided we have obtained your
+            informed consent;
+          </div>
+          <div>
+            ❌ Enable solely internal uses that are reasonably aligned with your
+            expectations based on your relationship with us;
+          </div>
+          <div>❌ Comply with an existing legal obligation; or</div>
+          <div>
+            ❌ Otherwise use your personal information, internally, in a lawful
+            manner that is compatible with the context in which you provided the
+            information.
+          </div>
+          <h2>Children Under 18</h2>
+          <p>
+            Bessa does not knowingly collect personally identifiable information
+            from children under the age of eighteen.
+          </p>
+          <h2>E-mail Communications</h2>
+          <p>
+            From time to time, Bessa may contact you via email for the purpose
+            of providing announcements, promotional offers, alerts,
+            confirmations, surveys, and/or other general communication.
+          </p>
+          <h2>Changes to this Statement</h2>
+          <p>
+            Bessa reserves the right to change this Privacy Policy from time to
+            time. We will notify you about significant changes in the way we
+            treat personal information by sending a notice to the primary email
+            address specified in your account, by placing a prominent notice on
+            our application, and/or by updating any privacy information. Your
+            continued use of the application and/or Services available after
+            such modifications will constitute your: (a) acknowledgment of the
+            modified Privacy Policy; and (b) agreement to abide and be bound by
+            that Policy.
+          </p>
+          <h2>Contact Information</h2>
+          <p>
+            Bessa welcomes your questions or comments regarding this Statement
+            of Privacy. If you believe that Bessa has not adhered to this
+            Statement, please contact Bessa at:
+          </p>
+          <div>topher@stupendousweb.com</div>
+          <div>(510) 944-0331</div>
+          <p>Effective as of February 08, 2023</p>
+        </div>
+      </div>
+    </>
+  );
+}
