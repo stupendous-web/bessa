e5534243da5b061f2cf78025217027b477e71775
stupendousweb <topher@stupendousweb.com>
2023-02-08 17:14:17 -0800

    Think twice, code once.

--- pages/terms.js
+++ pages/terms.js
@@ -18,12 +18,13 @@ export default function Terms() {
           <h1>Terms and Conditions</h1>
           <h2>Agreement between User and Bessa</h2>
           <p>
-            Welcome to Bessa. The Bessa website (the "Site") is comprised of
-            various web pages operated by Bessa. Bessa is offered to you
-            conditioned on your acceptance without modification of the terms,
-            conditions, and notices contained herein (the "Terms"). Your use of
-            Bessa constitutes your agreement to all such Terms. Please read
-            these terms carefully, and keep a copy of them for your reference.
+            Welcome to Bessa. The Bessa website (the &quot;Site&quot;) is
+            comprised of various web pages operated by Bessa. Bessa is offered
+            to you conditioned on your acceptance without modification of the
+            terms, conditions, and notices contained herein (the
+            &quot;Terms&quot;). Your use of Bessa constitutes your agreement to
+            all such Terms. Please read these terms carefully, and keep a copy
+            of them for your reference.
           </p>
           <h2>Electronic Communications</h2>
           <p>
@@ -54,14 +55,14 @@ export default function Terms() {
           </p>
           <h2>Links to Third Party Sites/Third Party Services</h2>
           <p>
-            Bessa may contain links to other websites ("Linked Sites"). The
-            Linked Sites are not under the control of Bessa and Bessa is not
-            responsible for the contents of any Linked Site, including without
-            limitation any link contained in a Linked Site, or any changes or
-            updates to a Linked Site. Bessa is providing these links to you only
-            as a convenience, and the inclusion of any link does not imply
-            endorsement by Bessa of the site or any association with its
-            operators.
+            Bessa may contain links to other websites (&quot;Linked
+            Sites&quot;). The Linked Sites are not under the control of Bessa
+            and Bessa is not responsible for the contents of any Linked Site,
+            including without limitation any link contained in a Linked Site, or
+            any changes or updates to a Linked Site. Bessa is providing these
+            links to you only as a convenience, and the inclusion of any link
+            does not imply endorsement by Bessa of the site or any association
+            with its operators.
           </p>
           <p>
             Certain services made available via Bessa are delivered by third
@@ -80,8 +81,8 @@ export default function Terms() {
             that you will not use the Site for any purpose that is unlawful or
             prohibited by these Terms. You may not use the Site in any manner
             which could damage, disable, overburden, or impair the Site or
-            interfere with any other party's use and enjoyment of the Site. You
-            may not obtain or attempt to obtain any materials or information
+            interfere with any other party&apos;s use and enjoyment of the Site.
+            You may not obtain or attempt to obtain any materials or information
             through any means not intentionally made available or provided for
             through the Site.
           </p>
@@ -116,9 +117,10 @@ export default function Terms() {
             groups, forums, communities, personal web pages, calendars, and/or
             other message or communication facilities designed to enable you to
             communicate with the public at large or with a group (collectively,
-            "Communication Services"). You agree to use the Communication
-            Services only to post, send and receive messages and material that
-            are proper and related to the particular Communication Service.
+            &quot;Communication Services&quot;). You agree to use the
+            Communication Services only to post, send and receive messages and
+            material that are proper and related to the particular Communication
+            Service.
           </p>
           <p>
             By way of example, and not as a limitation, you agree that when
@@ -132,9 +134,9 @@ export default function Terms() {
             publicity) unless you own or control the rights thereto or have
             received all necessary consents; upload files that contain viruses,
             corrupted files, or any other similar software or programs that may
-            damage the operation of another's computer; advertise or offer to
-            sell or buy any goods or services for any business purpose, unless
-            such Communication Service specifically allows such messages;
+            damage the operation of another&apos;s computer; advertise or offer
+            to sell or buy any goods or services for any business purpose,
+            unless such Communication Service specifically allows such messages;
             conduct or forward surveys, contests, pyramid schemes or chain
             letters; download any file posted by another user of a Communication
             Service that you know, or reasonably should know, cannot be legally
@@ -161,7 +163,7 @@ export default function Terms() {
             Bessa reserves the right at all times to disclose any information as
             necessary to satisfy any applicable law, regulation, legal process
             or governmental request, or to edit, refuse to post or to remove any
-            information or materials, in whole or in part, in Bessa's sole
+            information or materials, in whole or in part, in Bessa&apos;s sole
             discretion.
           </p>
           <p>
@@ -186,10 +188,10 @@ export default function Terms() {
             Bessa does not claim ownership of the materials you provide to Bessa
             (including feedback and suggestions) or post, upload, input or
             submit to any Bessa Site or our associated services (collectively
-            "Submissions"). However, by posting, uploading, inputting, providing
-            or submitting your Submission you are granting Bessa, our affiliated
-            companies and necessary sublicensees permission to use your
-            Submission in connection with the operation of their Internet
+            &quot;Submissions&quot;). However, by posting, uploading, inputting,
+            providing or submitting your Submission you are granting Bessa, our
+            affiliated companies and necessary sublicensees permission to use
+            your Submission in connection with the operation of their Internet
             businesses including, without limitation, the rights to: copy,
             distribute, transmit, publicly display, publicly perform, reproduce,
             edit, translate and reformat your Submission; and to publish your
@@ -199,7 +201,7 @@ export default function Terms() {
             No compensation will be paid with respect to the use of your
             Submission, as provided herein. Bessa is under no obligation to post
             or use any Submission you may provide and may remove any Submission
-            at any time in Bessa's sole discretion.
+            at any time in Bessa&apos;s sole discretion.
           </p>
           <p>
             By posting, uploading, inputting, providing or submitting your
@@ -222,7 +224,7 @@ export default function Terms() {
             You agree to indemnify, defend and hold harmless Bessa, its
             officers, directors, employees, agents and third parties, for any
             losses, costs, liabilities and expenses (including reasonable
-            attorney's fees) relating to or arising out of your use of or
+            attorney&apos;s fees) relating to or arising out of your use of or
             inability to use the Site or services, any user postings made by
             you, your violation of any terms of this Agreement or your violation
             of any rights of a third party, or your violation of any applicable
@@ -241,21 +243,21 @@ export default function Terms() {
             Federal Arbitration Act, conducted by a single neutral arbitrator
             and administered by the American Arbitration Association, or a
             similar arbitration service selected by the parties, in a location
-            mutually agreed upon by the parties. The arbitrator's award shall be
-            final, and judgment may be entered upon it in any court having
-            jurisdiction. In the event that any legal or equitable action,
-            proceeding or arbitration arises out of or concerns these Terms and
-            Conditions, the prevailing party shall be entitled to recover its
-            costs and reasonable attorney's fees. The parties agree to arbitrate
-            all disputes and claims in regards to these Terms and Conditions or
-            any disputes arising as a result of these Terms and Conditions,
-            whether directly or indirectly, including Tort claims that are a
-            result of these Terms and Conditions. The parties agree that the
-            Federal Arbitration Act governs the interpretation and enforcement
-            of this provision. The entire dispute, including the scope and
-            enforceability of this arbitration provision shall be determined by
-            the Arbitrator. This arbitration provision shall survive the
-            termination of these Terms and Conditions.
+            mutually agreed upon by the parties. The arbitrator&apos;s award
+            shall be final, and judgment may be entered upon it in any court
+            having jurisdiction. In the event that any legal or equitable
+            action, proceeding or arbitration arises out of or concerns these
+            Terms and Conditions, the prevailing party shall be entitled to
+            recover its costs and reasonable attorney&apos;s fees. The parties
+            agree to arbitrate all disputes and claims in regards to these Terms
+            and Conditions or any disputes arising as a result of these Terms
+            and Conditions, whether directly or indirectly, including Tort
+            claims that are a result of these Terms and Conditions. The parties
+            agree that the Federal Arbitration Act governs the interpretation
+            and enforcement of this provision. The entire dispute, including the
+            scope and enforceability of this arbitration provision shall be
+            determined by the Arbitrator. This arbitration provision shall
+            survive the termination of these Terms and Conditions.
           </p>
           <h2>Class Action Waiver</h2>
           <p>
@@ -263,13 +265,13 @@ export default function Terms() {
             an individual basis; class arbitrations and
             class/representative/collective actions are not permitted. THE
             PARTIES AGREE THAT A PARTY MAY BRING CLAIMS AGAINST THE OTHER ONLY
-            IN EACH'S INDIVIDUAL CAPACITY, AND NOT AS A PLAINTIFF OR CLASS
+            IN EACH&apos;S INDIVIDUAL CAPACITY, AND NOT AS A PLAINTIFF OR CLASS
             MEMBER IN ANY PUTATIVE CLASS, COLLECTIVE AND/ OR REPRESENTATIVE
             PROCEEDING, SUCH AS IN THE FORM OF A PRIVATE ATTORNEY GENERAL ACTION
             AGAINST THE OTHER. Further, unless both you and Bessa agree
-            otherwise, the arbitrator may not consolidate more than one person's
-            claims, and may not otherwise preside over any form of a
-            representative or class proceeding.
+            otherwise, the arbitrator may not consolidate more than one
+            person&apos;s claims, and may not otherwise preside over any form of
+            a representative or class proceeding.
           </p>
           <h2>Liability Disclaimer</h2>
           <p>
@@ -285,12 +287,13 @@ export default function Terms() {
             THE INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS
             CONTAINED ON THE SITE FOR ANY PURPOSE. TO THE MAXIMUM EXTENT
             PERMITTED BY APPLICABLE LAW, ALL SUCH INFORMATION, SOFTWARE,
-            PRODUCTS, SERVICES AND RELATED GRAPHICS ARE PROVIDED "AS IS" WITHOUT
-            WARRANTY OR CONDITION OF ANY KIND. BESSA AND/OR ITS SUPPLIERS HEREBY
-            DISCLAIM ALL WARRANTIES AND CONDITIONS WITH REGARD TO THIS
-            INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS,
-            INCLUDING ALL IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY,
-            FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT.
+            PRODUCTS, SERVICES AND RELATED GRAPHICS ARE PROVIDED &quot;AS
+            IS&quot; WITHOUT WARRANTY OR CONDITION OF ANY KIND. BESSA AND/OR ITS
+            SUPPLIERS HEREBY DISCLAIM ALL WARRANTIES AND CONDITIONS WITH REGARD
+            TO THIS INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED
+            GRAPHICS, INCLUDING ALL IMPLIED WARRANTIES OR CONDITIONS OF
+            MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+            NON-INFRINGEMENT.
           </p>
           <p>
             TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT SHALL
@@ -326,18 +329,18 @@ export default function Terms() {
           <p>
             You agree that no joint venture, partnership, employment, or agency
             relationship exists between you and Bessa as a result of this
-            agreement or use of the Site. Bessa's performance of this agreement
-            is subject to existing laws and legal process, and nothing contained
-            in this agreement is in derogation of Bessa's right to comply with
-            governmental, court and law enforcement requests or requirements
-            relating to your use of the Site or information provided to or
-            gathered by Bessa with respect to such use. If any part of this
-            agreement is determined to be invalid or unenforceable pursuant to
-            applicable law including, but not limited to, the warranty
-            disclaimers and liability limitations set forth above, then the
-            invalid or unenforceable provision will be deemed superseded by a
-            valid, enforceable provision that most closely matches the intent of
-            the original provision and the remainder of the agreement shall
+            agreement or use of the Site. Bessa&apos;s performance of this
+            agreement is subject to existing laws and legal process, and nothing
+            contained in this agreement is in derogation of Bessa&apos;s right
+            to comply with governmental, court and law enforcement requests or
+            requirements relating to your use of the Site or information
+            provided to or gathered by Bessa with respect to such use. If any
+            part of this agreement is determined to be invalid or unenforceable
+            pursuant to applicable law including, but not limited to, the
+            warranty disclaimers and liability limitations set forth above, then
+            the invalid or unenforceable provision will be deemed superseded by
+            a valid, enforceable provision that most closely matches the intent
+            of the original provision and the remainder of the agreement shall
             continue in effect.
           </p>
           <p>
