bf5534a2e2d7e974141e9a067d2ea8617abce888
stupendousweb <topher@stupendousweb.com>
2023-02-12 13:56:47 -0800

    Think twice, code once.

--- components/app/PostMedia.js
+++ components/app/PostMedia.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Link from "next/link";
 
 export default function PostMedia({ nSFW, type, id }) {
   const [filter, setFilter] = useState(true);
@@ -10,18 +9,47 @@ export default function PostMedia({ nSFW, type, id }) {
 
   return (
     <>
-      {type.includes("image") && (
+      {type.includes("image") ? (
         <div className={"uk-margin"}>
           <div className={"uk-width-1-1 uk-inline uk-cover-container"}>
-            <Link href={`/app/posts/${id}`}>
-              <img
-                src={`https://cdn.bessssssa.com/posts/${id}`}
-                className={"uk-width-1-1"}
-                style={{
-                  filter: filter ? "blur(75px)" : "blur()",
-                }}
-              />
-            </Link>
+            <img
+              src={`https://cdn.bessssssa.com/posts/${id}`}
+              className={"uk-width-1-1"}
+              style={{
+                filter: filter ? "blur(75px)" : "blur()",
+              }}
+            />
+            {filter && (
+              <>
+                <div className="uk-overlay-default uk-position-cover" />
+                <div className="uk-overlay uk-position-center uk-text-center">
+                  <p>This content is NSFW</p>
+                  <a
+                    className={"uk-button uk-button-default"}
+                    onClick={() => setFilter(false)}
+                  >
+                    Reveal
+                  </a>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className={"uk-margin"}>
+          <div className={"uk-width-1-1 uk-inline uk-cover-container"}>
+            <video
+              src={`https://cdn.bessssssa.com/posts/${id}`}
+              width={"1800"}
+              height={"1200"}
+              loop
+              muted
+              playsInline
+              data-uk-video={"autoplay: inview"}
+              style={{
+                filter: filter ? "blur(75px)" : "blur()",
+              }}
+            />
             {filter && (
               <>
                 <div className="uk-overlay-default uk-position-cover" />
