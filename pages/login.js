0d4030e261448ce48d3cff3ba42ac70ffe1fd244
stupendousweb <topher@stupendousweb.com>
2023-02-12 22:12:39 -0800

    Think twice, code once.

--- pages/login.js
+++ pages/login.js
@@ -16,12 +16,14 @@ export default function Login() {
       email: email,
       password: password,
       redirect: false,
-    }).then((response) => {
-      !response?.error
-        ? router.push("/app/posts")
-        : setError("Hmm. Something went wrong.");
-      console.log(response?.error);
-    });
+    })
+      .then((response) => {
+        !response?.error
+          ? router.push("/app/posts")
+          : setError("Hmm. Something went wrong.");
+        console.log(response?.error);
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
