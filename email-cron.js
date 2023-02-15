3df92bf2c59d54b59b74e04e59a8cdb9640464c6
stupendousweb <topher@stupendousweb.com>
2023-02-14 16:15:05 -0800

    Think twice, code once.

--- email-cron.js
+++
@@ -1,8 +0,0 @@
-const cron = require("node-cron");
-const { Logtail } = require("@logtail/node");
-const logtail = new Logtail("JjAcqTrRSTsZNb1fCSV9HQsW");
-
-cron.schedule("* * * * * *", function () {
-  logtail.info("cron triggered");
-  console.log("running a task every minute");
-});
