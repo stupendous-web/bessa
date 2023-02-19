18c12917667035f9e3ea1af5c4ab9241a6cc4548
stupendousweb <topher@stupendousweb.com>
2023-02-18 17:44:35 -0800

    Think twice, code once.

--- components/app/CommentButton.js
+++ components/app/CommentButton.js
@@ -12,6 +12,7 @@ export default function CommentButton({ postId }) {
     axios
       .post("/api/comments", {
         postId: postId,
+        comment: comment,
       })
       .then(() => {
         setComment("");
