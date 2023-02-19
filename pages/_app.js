7ee407b89f567ce4847b23167d45969c022985bd
stupendousweb <topher@stupendousweb.com>
2023-02-18 16:20:32 -0800

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import Head from "next/head";
+import Script from "next/script";
+import { useRouter } from "next/router";
 import { SessionProvider } from "next-auth/react";
 import UIkit from "uikit";
+import * as gtag from "../lib/gtag";
 
 import { Provider } from "@/lib/context";
 
@@ -12,6 +15,19 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     UIkit.container = ".uk-scope";
   });
@@ -29,7 +45,23 @@ export default function App({
     <>
       <Head>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <div className={"uk-scope"}>
         <SessionProvider session={session}>
           <Provider>
